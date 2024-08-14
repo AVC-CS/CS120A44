edb3ac65337666376f43506220feb02042b6333b
Kyu Woong Lee <kwooler@gmail.com>
2024-08-13 23:32:38 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,24 +4,21 @@
 using namespace std;
 int main()
 {
-        int selection;
+       int selection;
         cout << "Which formula do you want to see?\n\n";
-        cout << "1. Area of a circle\n";
-        cout << "2. Area of a rectangle\n";
-        cout << "3. Area of a cylinder\n";
-        cout << "4. None of them!\n";
+        cout << "1. Apple\n";
+        cout << "2. Bannana\n";
+        cout << "3. Orange\n" ;
+        cout << "4. None of them\n";
         cin >> selection;
-
-        //* leave the "cout" statement. Just change the if statement to "switch satement"
-
-        //*        if (selection == 1)
-        //*                cout << "Pi times radius squared\n";
-        //*        else if (selection == 2)
-        //*                cout << "Length times width\n";
-        //*        else if (selection == 3)
-        //*                cout << "Pi times radius squared times height\n";
-        //*        else if (selection == 4)
-        //*                cout << "Well okay then, good bye!\n";
-        //*        else
-        //*                cout < < < < "Not good with numbers, eh?\n";
+//*        if (selection == 1)
+//*                cout << "Menu 1 selected\n";
+//*        else if (selection == 2)
+//*                cout << "Menu 2 selected\n";
+//*        else if (selection == 3)
+//*                cout << "Menu 3 selected\n";
+//*        else if (selection == 4)
+//*                cout << "Menu 4 selected\n";
+//*        else
+//*                cout << "Wrong menu item\n";
 }
